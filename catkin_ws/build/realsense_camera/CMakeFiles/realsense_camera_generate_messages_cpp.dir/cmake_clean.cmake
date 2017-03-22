8470eaf5fc3cfa04b2ee5dde5094640e367118f9
Daniel Neumann <daniel.neumann@fu-berlin.de>
2017-03-22 14:32:39 +0100

    removed compiled files of catkin workspace

--- catkin_ws/build/realsense_camera/CMakeFiles/realsense_camera_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/realsense_camera_generate_messages_cpp"
-  "/root/catkin_ws/devel/.private/realsense_camera/include/realsense_camera/cameraConfiguration.h"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/realsense_camera_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- catkin_ws/build/realsense_camera/CMakeFiles/realsense_camera_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/realsense_camera_generate_messages_lisp"
-  "/root/catkin_ws/devel/.private/realsense_camera/share/common-lisp/ros/realsense_camera/srv/cameraConfiguration.lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/realsense_camera_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
