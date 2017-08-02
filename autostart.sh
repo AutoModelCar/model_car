5fb635bf5b23e37dc85ccf7313f64abbb3203480
D. Neumann <daniel.neumann@fu-berlin.de>
2017-08-02 19:53:30 +0200

    changed autostart.sh to use ROS environment variables from the .bashrc

--- autostart.sh
+++ autostart.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+modprobe uvcvideo
 
-### Set IP address for ROS
-#IP=`ifconfig wlan0 | grep "inet " | awk '{print $2}' | awk -F':' '{print $2}'` # automatically detect wifi address (TODO: ensure wifi is connected before)
-#IP=localhost		# Locally on odroid only
-#IP=192.168.1.199	# Ethernet
-IP=192.168.43.129	# Wifi
+# clear ROS log
+rm -rf /root/.ros/log
 
-### ROS Setup
-export ROS_HOSTNAME=$IP
-export ROS_MASTER_URI=http://$IP:11311
-source /opt/ros/indigo/setup.bash
-cd /root/catkin_ws
-source devel/setup.bash
+# load environment settings from .bashrc
+PS1='$ '
+source /root/.bashrc
+
+### ROS Setup (is now loaded from .bashrc)
+#export ROS_MASTER_URI=http://$IP:11311
+#...
+
+### Start ROS core and wait a few seconds
+roscore &
+sleep 5
 
 ### Start ROS launch scripts
 roslaunch manual_control manual_odroid.launch &
-#roslaunch random_movement auto.launch &
+#roslaunch random_movement auto.launch
+
+### Start Web Control Center
+#/root/web-control-center/start-server.sh &
 
